bc7fa5af176226042523929c463af5020d674409
Nikita <25131222+snitron@users.noreply.github.com>
2018-09-29 21:11:47 +0500

    First Upload

---
+++ Assets/Scripts/LoadNewLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewLevel : MonoBehaviour {
+    public string name;
+    public void LoadScene() {
+        SceneManager.LoadScene(name);
+    }
+}
